3c405bb75860dfb2833448e8a746b5f76bc3018a
postolenko <denis.postolenko@gmail.com>
2020-05-22 21:53:03 +0300

    SS

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
 bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
+var parentBlock, dropdownBox;
 
 $(window).load(function() {
 
@@ -25,33 +26,54 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
-            dots: true,
-            arrows: true,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            speed: 1200,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
+    // if( $(".portfolio_slider").length > 0 ) {
+    //     $(".portfolio_slider").not(".slick-initialized").slick({
+    //         dots: true,
+    //         arrows: true,
+    //         autoplay: true,
+    //         autoplaySpeed: 4000,
+    //         speed: 1200,
+    //         slidesToShow: 4,
+    //         slidesToScroll: 1,
+    //         // fade: true,
+    //         responsive: [
+    //             {
+    //               breakpoint: 900,
+    //               settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 2
+    //               }
+    //             },
+    //             {
+    //               breakpoint: 540,
+    //               settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //               }
+    //             }
+    //           ]
+    //     });
+    // }
+
+    $(".dropdown_item").each(function() {
+      if(!$(this).hasClass("active")) {
+        $(this).find(".dropdown_item_content").css({
+          "display" : "none"
         });
-    }
+      }
+    });
+
+    $(".dropdown_item_title").on("click", function(e) {
+      e.preventDefault();
+      parentBlock = $(this).closest(".dropdown_item");
+      dropdownBox = parentBlock.find(".dropdown_item_content");
+      if(dropdownBox.is(":hidden")) {
+        dropdownBox.slideDown(300);
+        parentBlock.addClass("active");
+      } else {
+        dropdownBox.slideUp(300);
+        parentBlock.removeClass("active");
+      }
+    });
 
 });
